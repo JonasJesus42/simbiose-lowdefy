245651c2db3e8ff6a86f8b895fe317e9bb53d39a
jonasjesus42 <jonasdasilvajesus@outlook.com>
2022-08-31 10:01:29 -0300

    feat: login

---
+++ public/login.js
@@ -0,0 +1,4 @@
+function login(email, password, emailRequest, passwordRequest) {
+  console.log(email, password, emailRequest, passwordRequest)
+}
+window.lowdefy.registerJsOperator('login', login);
